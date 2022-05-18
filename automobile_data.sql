fc2f16398c021d506d4a2f2ddd0b5ae4daa97d93
Uma Negi <89074181+umanegi8@users.noreply.github.com>
2022-05-18 16:26:58 +0530

    Update automobile_data.sql

--- automobile_data.sql
+++ automobile_data.sql
@@ -1,11 +1,3 @@
-/* In the scenario, you are a data analyst working with a used car dealership startup venture. 
-The investors want you to find out which cars are most popular with customers so they can make sure to stock accordingly.  
-This dataset contains historical sales data, including details such as car features and prices. 
-Use the data to find the top 10 most popular cars and trims. Before analyzing we need to clean data.
-If analyze dirty data, one could end up presenting the wrong list of cars to the investors. 
-That may cause them to lose money on their car inventory investment.
-*/
-
 SELECT * FROM coursera.automobile_data;
 
 -- Step 1: Inspect the fuel_type column (should contain only 2 ypes of fuel)
